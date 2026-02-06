31b9a6bc7f115b933e17c5aa13d82f61adee1658
Tchapwo1 <paris2london@gmail.com>
2026-02-06 02:12:14 +0000

    Refactor Eleventy config and add layouts support
    Refactor Eleventy configuration to improve readability and add layouts directory.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,18 @@
 const { DateTime } = require("luxon");
-module.exports = function(eleventyConfig) {
+
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
-  eleventyConfig.addFilter("readableDate", d => DateTime.fromJSDate(d).toFormat("d LLL yyyy"));
-  return { dir: { input: "src", includes: "_includes", output: "_site" } };
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat("d LLL yyyy");
+  });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      layouts: "_includes/layouts",
+      output: "_site"
+    }
+  };
 };
